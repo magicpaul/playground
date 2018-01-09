d8d8740201e79bf1a02219b6b0dc0f0108743fde
magicpaul <paul@Pauls-MBP.home>
2018-01-09 22:33:34 +0000

    contact fix

--- src/pages/greatest-story/index.js
+++ src/pages/greatest-story/index.js
@@ -20,6 +20,11 @@ const Article = () => (
 
 const Homework = () => (
   <DevHomework title="Learn More">
+    <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <textarea name="message"></textarea>
+    </form>
     <ContactForm />
   </DevHomework>
 );
